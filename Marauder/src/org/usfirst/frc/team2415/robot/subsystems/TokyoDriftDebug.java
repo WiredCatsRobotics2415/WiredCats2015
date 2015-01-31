49a99f62f4a5d1d44e6c2c2724926dca48b30cda
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2015-01-31 15:56:25 -0500

    Need To figure out local issues

---
+++ Marauder/src/org/usfirst/frc/team2415/robot/subsystems/TokyoDriftDebug.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2415.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.I2C;
+
+
+
+public class TokyoDriftDebug extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
