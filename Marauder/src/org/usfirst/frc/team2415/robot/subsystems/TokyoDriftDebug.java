b556d710e98b4175effb1d3796acd90dbe5da50d
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2015-02-01 14:20:50 -0500

    Added Command for TokyoDrifit Debot.

--- Marauder/src/org/usfirst/frc/team2415/robot/subsystems/TokyoDriftDebug.java
+++ Marauder/src/org/usfirst/frc/team2415/robot/subsystems/TokyoDriftDebug.java
@@ -1,21 +1,25 @@
 package org.usfirst.frc.team2415.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2415.robot.commands.Nitro;
 
+import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.I2C;
 
-
+  
 
 public class TokyoDriftDebug extends Subsystem {
-    
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	//Commit N
-	
+	public TokyoDriftDebug(){
+		
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new Nitro());
     }
 }
 
