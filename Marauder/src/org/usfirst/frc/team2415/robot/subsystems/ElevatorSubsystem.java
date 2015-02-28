384ce44faa050c0d7f9db72a0627de3706e52235
Jabari Booker <booker.oronde@gmail.com>
2015-02-28 08:39:21 -0500

    Update ElevatorSubsystem.java
    Updating branch to match local branches

--- Marauder/src/org/usfirst/frc/team2415/robot/subsystems/ElevatorSubsystem.java
+++ Marauder/src/org/usfirst/frc/team2415/robot/subsystems/ElevatorSubsystem.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
+import org.usfirst.frc.team2415.robot.PID;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
@@ -17,8 +18,10 @@ public class ElevatorSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
+	private PID elevatorJiggler;
+
 	private final int TICKS_PER_INCH = 285;
-			
+
 	private Encoder encoder;
 	
 	private CANTalon talon1, talon2;
@@ -30,6 +33,8 @@ public class ElevatorSubsystem extends Subsystem {
 	private DoubleSolenoid elevatorBreak;
 	
 	public ElevatorSubsystem(){
+		elevatorJiggler = new PID(1);   //this isn't tuned yet
+		
 		talon1 = new CANTalon(RobotMap.ELEVATOR_CAN_TALONS[0]);
 		talon2 = new CANTalon(RobotMap.ELEVATOR_CAN_TALONS[1]);
 		
@@ -46,59 +51,24 @@ public class ElevatorSubsystem extends Subsystem {
     	setDefaultCommand(new ElevateRecycleBinCommand());
     }
     
-    /**Moves elevator up.*/
-    public void up(double speed){
+    public void setMotors(double speed){
     	//elevatorBreak.set(DoubleSolenoid.Value.kReverse);
     	talon1.set(speed);
     	talon2.set(speed);
     }
+    
+    //moves elevator down from tote elevation
+    public void downFromTote(){}
 
-    /**Move elevator down*/
-    public void Down(){
-    	elevatorBreak.set(DoubleSolenoid.Value.kReverse);
-    	talon1.set(-1);
-    	talon2.set(-1);
+    //moves elevator down from recycling bin elevation
+    public void downFromBin(){}
+    
+    public double getPidOut(double desired){
+    	return elevatorJiggler.pidOutput(getEncoder(), (float)desired);
     }
     
-    /**Returns count of the elevator hall effect sensor
-     * @return <b>int</b> ticks counted each time the <br> elevator passes the height of the <br>
-     * tote.*/
-    public boolean getEleHall(){
-    	return eleHall.get();
-    }
-    
-    /**Resets the count of the elevator hall effect sensor.*/
-    public void resetHall(){
-    	//eleHall.reset();
-    }
-    
-    /**Stops the movement of the elevator.*/
-    public void stop(){
-    	talon1.set(0);
-    	talon2.set(0);
-    	elevatorBreak.set(DoubleSolenoid.Value.kForward);
-    }
-    
-    /**Activates the elevator's pushing solenoids to push the tote.*/
-    public void pushTote(){
-    	toteNoid.set(DoubleSolenoid.Value.kForward);
-    }
-    
-    /**Retracts the elevator's pushing solenoids.*/
-    public void retractToteNoid(){
-    	toteNoid.set(DoubleSolenoid.Value.kReverse);
-    }
-    
-    /**Reset the tick count of the elevator encoder to 0.*/
-    public void resetEncoder(){
-    	encoder.reset();
-    }
-    
-    /**Returns the current height of the elevator
-     * 	@return <b>float</b> current height of the elevator in inches
-     * */
-    public float getHeight(){
-    	return encoder.get();// * TICKS_PER_INCH;
+    public int getEncoder(){
+    	return encoder.get();
     }
 }
 
